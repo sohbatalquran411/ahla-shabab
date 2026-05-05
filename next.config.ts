3897e4382587789428ffbc45b3553c9d297afe30
sohbatalquran411 <sohbatalquran411@gmail.com>
2026-05-06 01:20:14 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === "production" ? "/ahla-shabab" : "",
 };
 
 export default nextConfig;
